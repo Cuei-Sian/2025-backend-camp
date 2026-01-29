959c78b15afbcc3312337fde120166e6dcfe3ce7
doreen <doreen20080615@gmail.com>
2026-01-30 01:19:46 +0800

    更新:初階任務

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,10 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/healthcheck", (req, res) => {
-  res.status(200).json({ status: "success", message: "OK" });
+  res.status(200).json({
+    status: "success",
+    message: "OK",
+  });
 });
 app.use("/api/credit-package", creditPackageRouter);
 app.use("/api/coaches/skill", skillRouter);
